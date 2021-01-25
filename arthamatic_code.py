d30e992b23a5e085dda314cac338c8645d9bd066
nagababu124 <74544848+nagababu124@users.noreply.github.com>
2021-01-25 20:13:20 +0530

    Create arthamatic_code.py

---
+++ arthamatic_code.py
@@ -0,0 +1,11 @@
+# my_first_program
+a=10;
+b=5;
+add = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+print ("addition of two numbers: ",add)
+print ("substraction of two numbers: ",sub)
+print ("multiplication of two numbers: ",mul)
+print ("division of two numbers: ",div)
